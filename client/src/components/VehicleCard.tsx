3dd2c8fc5b327e57cdf8f5c5de2736d8eedda635
Diogo Agostinho <laagostim.oficial@gmail.com>
2024-03-06 20:48:39 +0000

    v05 - More vehicles

--- client/src/components/VehicleCard.tsx
+++ client/src/components/VehicleCard.tsx
@@ -16,13 +16,14 @@ function VehicleCard(props: {
     >
       <CardBody className="p-0 flex-row h-full">
         <img className="object-cover w-1/2" src={`/thumbs/${props.image}`} />
-        <div className="w-1/2 flex flex-col p-6">
+        <div className="w-1/2 flex flex-col justify-between p-6">
           <div className="flex flex-col">
             <p className="font-normal pb-1 text-lg">{props.fuel}</p>
             <p className="font-black text-xl">{props.name}</p>
             <p className="font-semibold text-lg">{props.description}</p>
           </div>
           <div className="flex flex-col pt-8">
+            <p className="font-semibold text-sm">Starting at:</p>
             <div className="flex gap-6 pb-2">
               <p>{props.price}</p>
               <p>{props.horsepower}</p>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -16,19 +16,103 @@ function Home() {
         </h1>
         <div>
           <Tabs
-            className="flex justify-center text-center pt-6"
+            className="flex justify-center text-center pt-6 "
             variant="underlined"
           >
-            <Tab className="text-lg" key="cars" title="Cars"></Tab>
-            <Tab className="text-lg" key="trucks" title="Trucks" />
+            <Tab className="text-lg" key="cars" title="Cars">
+              <div className="flex flex-row flex-wrap gap-6 justify-center px-10 sm:px-10 xl:px-10 2xl:px-40">
+                <VehicleCard
+                  image={"yaris.jpg"}
+                  fuel={"Gas | Hybrid"}
+                  name={"Yaris"}
+                  description={"Compact and agile"}
+                  price={"$20,300"}
+                  horsepower={"92hp"}
+                  link={"/vehicles/yaris"}
+                />
+                <VehicleCard
+                  image={"aygo.jpg"}
+                  fuel={"Gas | Hybrid"}
+                  name={"Aygo X"}
+                  description={"Elevated, for you"}
+                  price={"$18,240"}
+                  horsepower={"71hp"}
+                  link={"/vehicles/aygo"}
+                />
+                <VehicleCard
+                  image={"camry.jpg"}
+                  fuel={"Gas | Hybrid"}
+                  name={"Camry"}
+                  description={"Discover more drive for every day"}
+                  price={"$26,420"}
+                  horsepower={"203hp"}
+                  link={"/vehicles/camry"}
+                />
+                <VehicleCard
+                  image={"corolla.jpg"}
+                  fuel={"Gas | Hybrid"}
+                  name={"Corolla"}
+                  description={"Refine your routine"}
+                  price={"$21,700"}
+                  horsepower={"169hp"}
+                  link={"/vehicles/corolla"}
+                />
+                <VehicleCard
+                  image={"mirai.jpg"}
+                  fuel={"Hydrogen"}
+                  name={"Mirai"}
+                  description={"Innovation is power"}
+                  price={"$49,500"}
+                  horsepower={"182hp"}
+                  link={"/vehicles/mirai"}
+                />
+                <VehicleCard
+                  image={"crown.jpg"}
+                  fuel={"Hybrid"}
+                  name={"Crown"}
+                  description={"An image of innovation"}
+                  price={"$40,350"}
+                  horsepower={"236hp"}
+                  link={"/vehicles/crown"}
+                />
+              </div>
+            </Tab>
+
+            <Tab className="text-lg" key="trucks" title="Trucks">
+              <div className="flex flex-row flex-wrap gap-6 justify-center px-10 sm:px-10 xl:px-10 2xl:px-40">
+                <VehicleCard
+                  image={"tacoma.jpg"}
+                  fuel={"Gas"}
+                  name={"Tacoma"}
+                  description={"The adventure never ends"}
+                  price={"$28,600"}
+                  horsepower={"278hp"}
+                  link={"/vehicles/tacoma"}
+                />
+                <VehicleCard
+                  image={"tundra.jpg"}
+                  fuel={"Gas"}
+                  name={"Tundra"}
+                  description={"Designed to be tough"}
+                  price={"$38,965"}
+                  horsepower={"358hp"}
+                  link={"/vehicles/tundra"}
+                />
+                <VehicleCard
+                  image={"hilux.jpg"}
+                  fuel={"Gas"}
+                  name={"Hilux"}
+                  description={"The unbreakable machine"}
+                  price={"$39,900"}
+                  horsepower={"201hp"}
+                  link={"/vehicles/hilux"}
+                />
+              </div>
+            </Tab>
             <Tab className="text-lg" key="suvs" title="SUVs" />
-            <Tab
-              className="text-lg"
-              key="alternatives"
-              title="Electric & Hybrid"
-            />
+            <Tab className="text-lg" key="electric" title="100% Electric" />
             <Tab className="text-lg" key="gr" title="GR">
-              <div className="flex flex-row flex-wrap gap-4 justify-center">
+              <div className="flex flex-row flex-wrap gap-6 justify-center px-10 sm:px-10 xl:px-10 2xl:px-40">
                 <VehicleCard
                   image={"gr86.jpg"}
                   fuel={"Gas"}
@@ -48,23 +132,21 @@ function Home() {
                   link={"/vehicles/gr-yaris"}
                 />
                 <VehicleCard
-                  image={"gryaris.jpg"}
+                  image={"grcorolla.jpg"}
                   fuel={"Gas"}
                   name={"GR Corolla"}
-                  description={
-                    "Built for the track, ready for every day driving"
-                  }
-                  price={"$40,000"}
-                  horsepower={"118hp"}
+                  description={"Ready to rally"}
+                  price={"$35,900"}
+                  horsepower={"300hp"}
                   link={"/vehicles/gr-corolla"}
                 />
                 <VehicleCard
-                  image={"gryaris.jpg"}
+                  image={"grsupra.jpg"}
                   fuel={"Gas"}
                   name={"GR Supra"}
-                  description={"A legend reborn"}
-                  price={"$40,000"}
-                  horsepower={"118hp"}
+                  description={"Shift it up a notch"}
+                  price={"$44,640"}
+                  horsepower={"255hp"}
                   link={"/vehicles/gr-supra"}
                 />
               </div>

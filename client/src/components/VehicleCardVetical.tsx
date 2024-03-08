6b3051025bcd025e51a7e3af84bbc9124f9147bf
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-03-08 17:47:19 +0000

    v09 - Vehicles page

---
+++ client/src/components/VehicleCardVetical.tsx
@@ -0,0 +1,47 @@
+import { Card, CardBody, Button, Link } from "@nextui-org/react";
+
+function VehicleCardVertical(props: {
+  image: string;
+  fuel: string[];
+  name: string;
+  description: string;
+  price: string;
+  horsepower: string;
+  link: string;
+}) {
+  let splitFuel = props.fuel.toString();
+
+  if (props.fuel.length > 1) {
+    splitFuel = splitFuel.split(",").join(" | ");
+  }
+
+  return (
+    <Card
+      shadow="md"
+      className="border-none bg-background/60 dark:bg-default-100/50 max-w-[550px] max-h-[500px]"
+    >
+      <CardBody className="p-0 flex-row h-full">
+        <img className="object-cover w-1/2" src={props.image} />
+        <div className="w-1/2 flex flex-col justify-between p-6">
+          <div className="flex flex-col">
+            <p className="font-normal pb-1 text-lg">{splitFuel}</p>
+            <p className="font-black text-xl">{props.name}</p>
+            <p className="font-semibold text-lg">{props.description}</p>
+          </div>
+          <div className="flex flex-col pt-8">
+            <p className="font-semibold text-sm">Starting at:</p>
+            <div className="flex gap-6 pb-2">
+              <p>${props.price}</p>
+              <p>{props.horsepower}hp</p>
+            </div>
+            <Link href={props.link}>
+              <Button className="font-semibold">See More</Button>
+            </Link>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default VehicleCardVertical;
--- client/src/pages/Vehicles.tsx
+++ client/src/pages/Vehicles.tsx
@@ -1,10 +1,79 @@
+import {
+  Dropdown,
+  DropdownTrigger,
+  Button,
+  DropdownMenu,
+  DropdownItem,
+  Select,
+  SelectItem,
+} from "@nextui-org/react";
+import { useMemo, useState } from "react";
 import { useEffect } from "react";
 
 function Vehicles() {
   useEffect(() => {
-    document.title = "Toyota - All Vehicles";
+    document.title = "Toyota - Vehicles";
   });
-  return <div>Vehicles</div>;
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    new Set(["All Vehicles"])
+  );
+
+  const selectedValue = useMemo(
+    () => Array.from(selectedCategory).join(", ").replaceAll("_", " "),
+    [selectedCategory]
+  );
+
+  return (
+    <>
+      <div className="py-8">
+        <h1 className="text-4xl font-bold text-center">Toyota Vehicles</h1>
+      </div>
+      <div className="px-20 justify-center">
+        <div className="flex justify-between align-middle items-center pb-6">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="bordered" className="capitalize">
+                {selectedValue}
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Single selection example"
+              variant="flat"
+              disallowEmptySelection
+              selectionMode="single"
+              selectedKeys={selectedCategory}
+              onSelectionChange={setSelectedCategory}
+            >
+              <DropdownItem key="All Vehicles">All Vehicles</DropdownItem>
+              <DropdownItem key="Cars">Cars</DropdownItem>
+              <DropdownItem key="Trucks">Trucks</DropdownItem>
+              <DropdownItem key="SUVs">SUVs</DropdownItem>
+              <DropdownItem key="Alternative Fuel">
+                Alternative Fuel
+              </DropdownItem>
+              <DropdownItem key="GR">GR</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+
+          <Select size="sm" label="Order By:" className="max-w-xs">
+            <SelectItem key="a-z" value="a-z">
+              A-Z
+            </SelectItem>
+            <SelectItem key="highest" value="highest">
+              Highest Price
+            </SelectItem>
+            <SelectItem key="lowest" value="lowest">
+              Lowest Price
+            </SelectItem>
+            <SelectItem key="horsepower" value="horsepower">
+              Horsepower
+            </SelectItem>
+          </Select>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Vehicles;

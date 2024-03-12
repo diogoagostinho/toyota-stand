ede5b47925e3a801c854d4206e90ebeeac12acf3
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-03-12 12:12:46 +0000

    v13 - Update

--- client/src/pages/VehiclePage.tsx
+++ client/src/pages/VehiclePage.tsx
@@ -17,23 +17,19 @@ function VehiclePage() {
     <>
       {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
       {car.map((item: any) => (
-        <div
-          className={`w-full bg-center bg-cover bg-[url(${item.bannerImage})]`}
-        >
-          <div className="flex items-start justify-start w-full h-full bg-gray-900 bg-opacity-50 py-20">
-            <div className="text-left">
-              <div className="container px-4">
-                <div className="max-w-4xl text-left">
-                  <h2 className="mt-8 mb-6 text-2xl lg:text-2xl font-bold text-gray-100">
-                    Toyota {item.name}
-                  </h2>
-                  <p className=" max-w-md mb-10 text-md font-semibold text-gray-300">
-                    {item.description}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div>
+          <img src={item.bannerImage} />
+          <p>Toyota {item.name}</p>
+          <p>{item.price}</p>
+          <p>{item.horsepower}</p>
+          <p>{item.bigDesc}</p>
+          <p>{item.smallDesc}</p>
+          <p>{item.fuel}</p>
+          <img src={item.ctaImage1} />
+          <img src={item.ctaImage2} />
+          <img src={item.exteriorImage} />
+          <img src={item.interiorImage} />
+          <img src={item.modelImage} />
         </div>
       ))}
     </>

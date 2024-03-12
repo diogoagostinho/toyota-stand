ab2722bf5eec45ba0cd2fd1365e78b981f61d6ae
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-03-12 17:45:29 +0000

    v15

--- client/src/pages/VehiclePage.tsx
+++ client/src/pages/VehiclePage.tsx
@@ -29,21 +29,36 @@ function VehiclePage() {
   return (
     <>
       {car.map((item: any) => (
-        <div>
-          <div>
-            <img src={item.bannerImage} />
+        <div key={item.nameId}>
+          <div
+            style={{ "--image-url": `url(${item.bannerImage})` }}
+            className={"w-full bg-center bg-cover bg-[image:var(--image-url)]"}
+          >
+            <div className="flex items-start justify-start w-full h-full pt-64 pb-10 pl-4">
+              <div className="text-left">
+                <div className="container px-4 mx-auto ">
+                  <div className="max-w-4xl text-white mx-auto text-left drop-shadow-2xl">
+                    <h2 className="text-4xl pb-3 font-bold drop-shadow-xl leading-10 tracking-tight sm:leading-none">
+                      Toyota {item.name}
+                    </h2>
+                    <p className="drop-shadow-xl text-xl">{item.description}</p>
+                    <p className="drop-shadow-xl text-md">
+                      Starting at ${item.price} / {item.horsepower}hp
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <p>Toyota {item.name}</p>
-          <p>{item.price}</p>
-          <p>{item.horsepower}</p>
-          <p>{item.bigDesc}</p>
-          <p>{item.smallDesc}</p>
-          <p>{item.fuel}</p>
-          <img src={item.ctaImage1} />
-          <img src={item.ctaImage2} />
-          <img src={item.exteriorImage} />
-          <img src={item.interiorImage} />
-          <img src={item.modelImage} />
+          <div className="bg-red-500 bg-opacity-20 rounded-xl m-10 px-20 py-20 text-center flex flex-col justify-center items-center gap-4">
+            <h1 className="text-4xl font-bold">{item.bigDesc}</h1>
+            <p className="text-xl font-normal w-1/2">{item.smallDesc}</p>
+            <img
+              className="w-[500px] pt-10"
+              src={`/models/${item.nameId}.png`}
+              alt=""
+            />
+          </div>
         </div>
       ))}
     </>
